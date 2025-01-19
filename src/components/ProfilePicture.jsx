e4077186b2c586f5ebe9daa26e185201362406f1
910jmiguel <910jmiguel@gmail.com>
2025-01-19 00:53:19 -0500

    pic fix take 2

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProfilePicture = () => {
   return (
     <div>
-        <img className="w-43 h-40 mx-auto rounded-full" src="assets\IMG_3140.jpg" alt="miguel profile picture" />
+        <img className="w-43 h-40 mx-auto rounded-full" src="/assets/IMG_3140.jpg" alt="miguel profile picture" />
     </div>
   )
 }
