d0e6675eaf2928c8850b7cb7e8e5aa9f8c259e6d
910jmiguel <910jmiguel@gmail.com>
2025-02-17 10:49:04 -0500

    edited about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,7 +5,9 @@ const AboutSection = () => {
     <div className="text-gray-300 pt-5 mx-auto max-w-lg pb-10">
         <p>Hey, I'm Miguel👋! Born and raised in the Toronto area, I'm currently a 2nd year Computer Science student at York University
            who is passionate about full-stack development and software engineering, constantly exploring new technologies to create innovative applications.
-        </p>
+        </p> <br></br>
+
+        <p><strong>NOTE: </strong>I'm still in the process of working on this profile card and an official portfolio page coming soon :)</p>
     </div>
   )
 }
